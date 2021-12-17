c173dd5ce5eee038fe3c5a045c7342235f0abf9f
IDRIMalek <idri.malek@gmail.com>
2021-12-17 05:49:55 +0000

    mise à jour du 17 dec

--- DOCKER_eval/setup.sh
+++ DOCKER_eval/setup.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+echo "==========Création des images"
 docker image build ~/DOCKER_eval/authentication -t authentication:latest 
 #docker image build ~/DOCKER_eval/authorization -t authorization:latest 
 #docker image build ~/DOCKER_eval/content -t content:latest 
-touch log.txt
+
+touch log.txt  
+echo "==========Containerisation="
 docker-compose up >> log.txt 
+echo "==========fin du process==="
