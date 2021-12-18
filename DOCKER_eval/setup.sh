f801921408546a1d0ea846be937d8da95db77b48
IDRIMalek <idri.malek@gmail.com>
2021-12-18 06:13:16 +0000

    mise à jour des scripts python (nom correct des tests)

--- DOCKER_eval/setup.sh
+++ DOCKER_eval/setup.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-docker network create --subnet 172.50.0.0/16 --gateway 172.50.0.1 my_network
-
 docker image build ~/DOCKER_eval/authentication -t authentication:latest 
 docker image build ~/DOCKER_eval/authorization -t authorization:latest 
-#docker image build ~/DOCKER_eval/content -t content:latest 
+docker image build ~/DOCKER_eval/content -t content:latest 
 
 touch log.txt  
 docker-compose up >> log.txt 
+
